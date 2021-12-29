6700c095976e64a2c129c897051c32ad1ccfd7b0
Maria Manoela <manoelamvc@hotmail.com>
2021-12-29 09:38:13 -0300

    Add CounterFunction to App function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import CounterFunction from './components/counterFunction';
 
 function App() {
   return (
-   
+    <>
+      <CounterFunction/>
+      <CounterFunction/>
+    </>
   );
 }
 
