422410a83ca5d699202859398ebfce9660c80643
Maria Manoela <manoelamvc@hotmail.com>
2021-12-30 10:46:00 -0300

    Implement helloFunctionComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import CounterFunction from './components/counterFunction';
+import HelloFunctionComponent from './components/helloFunctionComponent';
 
 function App() {
   return (
     <>
       <CounterFunction initialValue={10} />
+      <HelloFunctionComponent name = "Manu" age = {21}/>
+      <HelloFunctionComponent name = "Fulano"/>
     </>
   );
 }
