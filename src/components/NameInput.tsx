07cb4545df4c4a3c9cc674eb6314deb3b0a06008
Maria Manoela <manoelamvc@hotmail.com>
2021-12-30 11:57:42 -0300

    Add handleChange function

--- src/components/NameInput.tsx
+++ src/components/NameInput.tsx
@@ -1,11 +1,17 @@
 import React, {useState, useEffect} from "react";
 
 function NameInput() {
+    const [name, setName] = useState('valor inicial');
     console.log('rendered');
+
+    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        setName(event.target.value);
+    };
+
     return (
         <>
             <label htmlFor="nameInput">Name: </label>
-            <input id="nameInput"/>
+            <input id="nameInput" value={name} onChange={handleChange}/>
         </>
     )
 }
