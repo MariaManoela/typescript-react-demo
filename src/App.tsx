4f18fd56c38519fc150f609935b6d4775ed0b039
Maria Manoela <manoelamvc@hotmail.com>
2021-12-30 11:49:09 -0300

    Implement NameInput function

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import CounterFunction from './components/counterFunction';
 import HelloFunctionComponent from './components/helloFunctionComponent';
+import NameInput from './components/NameInput';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <CounterFunction initialValue={10} />
       <HelloFunctionComponent name = "Manu" age = {21}/>
       <HelloFunctionComponent name = "Fulano"/>
+      <NameInput/>
     </>
   );
 }
