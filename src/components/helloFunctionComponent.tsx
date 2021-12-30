40b84e7d8f90dbb2d73098d80bb9918267ec95c0
Maria Manoela <manoelamvc@hotmail.com>
2021-12-30 10:42:01 -0300

    Finish function

---
+++ src/components/helloFunctionComponent.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface HelloProps {
+    name: string,
+    age?: number
+}
+
+function HelloFunctionComponent(props: HelloProps) {
+    const ageText = props.age ? `You are ${props.age} years old` : 'How old are you?';
+    return (
+        <div>
+            Hello {props.name}<br/>
+            {ageText}
+        </div>
+    );
+}
+
+export default HelloFunctionComponent;
